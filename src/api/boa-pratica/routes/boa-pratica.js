afc6041b5478af880f93f353c7d0404cad1bdef4
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-07-15 13:54:08 -0400

    mudando componente de boas praticas

---
+++ src/api/boa-pratica/routes/boa-pratica.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * boa-pratica router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::boa-pratica.boa-pratica');
