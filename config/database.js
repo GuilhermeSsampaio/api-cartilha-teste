3ccfa630588e1238d592d5e1203c4d85b93e3217
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-08-16 14:41:34 -0400

    mudando db pra supabase

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
-module.exports = ({env}) => ({
-  connection:{
+// module.exports = ({env}) => ({
+//   connection:{
+//     client: 'postgres',
+//     connection:{
+//         host: env('DATABASE_HOST'),
+//         port: env('DATABASE_PORT'),
+//         database: env('DATABASE_NAME'),
+//         user: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//         ssl: env.bool('DATABASE_SSL')
+//     }
+// //
+//   }
+
+// })
+module.exports = ({ env }) => ({
+  connection: {
     client: 'postgres',
-    connection:{
-        host: env('DATABASE_HOST'),
-        port: env('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL')
-    }
-
-  }
-
-})
+    connection: env('DATABASE_URL', {
+      ssl: env.bool('DATABASE_SSL', false),
+    }),
+  },
+});
