03dd2e6a5b45dd5e6c69d17ac328303bd7f632dd
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-08-19 11:15:06 -0400

    nova api boas praticas

---
+++ src/api/boa-pratica/services/boa-pratica.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * boa-pratica service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::boa-pratica.boa-pratica');
