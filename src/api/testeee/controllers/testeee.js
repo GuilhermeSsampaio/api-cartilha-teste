71e1cb36956f52643b32b2f0773b4ae82d074549
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-09-12 16:09:18 -0400

    removendo draft

--- src/api/testeee/controllers/testeee.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * testeee controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::testeee.testeee');
--- src/api/testeee/routes/testeee.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * testeee router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::testeee.testeee');
