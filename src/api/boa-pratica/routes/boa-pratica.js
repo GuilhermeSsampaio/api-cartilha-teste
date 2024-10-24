747f43b0dd0b8536014043cd1ed258e54d501821
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-10-24 11:23:37 -0400

    subindo mudanças de nomes das coleções

--- src/api/boa-pratica/routes/boa-pratica.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * boa-pratica router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::boa-pratica.boa-pratica');
--- src/api/boa-pratica/services/boa-pratica.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * boa-pratica service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::boa-pratica.boa-pratica');
