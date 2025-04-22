e547fbff1d19647c0751597e661d3552f0698313
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-04-22 09:57:36 -0400

    tentando

--- config/database.js
+++ config/database.js
@@ -1,23 +1,22 @@
-// module.exports = ({env}) => ({
-//   connection:{
-//     client: 'postgres',
-//     connection:{
-//         host: env('DATABASE_HOST'),
-//         port: env('DATABASE_PORT'),
-//         database: env('DATABASE_NAME'),
-//         user: env('DATABASE_USERNAME'),
-//         password: env('DATABASE_PASSWORD'),
-//         ssl: env.bool('DATABASE_SSL')
-//     }
-// //
-//   }
-
-// })
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
-    connection: env('DATABASE_URL', {
-      ssl: env.bool('DATABASE_SSL', false),
-    }),
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
+    },
+    //
   },
 });
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: env("DATABASE_URL", {
+//       ssl: env.bool("DATABASE_SSL", false),
+//     }),
+//   },
+// });
